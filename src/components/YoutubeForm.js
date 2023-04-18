4e5b41ba456f92f4dd011dea025236c9405f5b31
Akshay7016 <asajgule@gmail.com>
2023-04-18 10:39:41 +0530

    Form Validation added

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -24,7 +24,14 @@ const YoutubeForm = () => {
                     <input
                         type="text"
                         id="username"
-                        {...register("username")} />
+                        {...register("username", {
+                            required: "Username is required",
+                            minLength: {
+                                value: 5,
+                                message: "username minimum length should be 5"
+                            }
+                        })}
+                    />
                 </div>
 
                 <div className='label-container'>
@@ -32,7 +39,30 @@ const YoutubeForm = () => {
                     <input
                         type="email"
                         id="email"
-                        {...register("email")} />
+                        {...register("email", {
+                            // Validation
+                            required: {
+                                value: true,
+                                message: "Email is required"
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/,
+                                message: "invalid email format"
+                            },
+
+                            //custom validation
+                            validate: {
+                                notAdmin: (fieldValue) => {
+                                    return fieldValue !== "admin@example.com" || "Enter a different email address"
+                                },
+
+                                notBlackListed: (fieldValue) => {
+                                    return !fieldValue.endsWith("baddomain.com") || "This domain is not supported"
+                                }
+                            }
+                        }
+                        )}
+                    />
                 </div>
 
                 <div className='label-container'>
@@ -40,7 +70,12 @@ const YoutubeForm = () => {
                     <input
                         type="text"
                         id="channel"
-                        {...register("channel")}
+                        {...register("channel", {
+                            required: {
+                                value: true,
+                                message: "Channel name is required"
+                            }
+                        })}
                     />
                 </div>
 
